181fc17b37e39ce7f0efa053f2db5e8cc548f15f
Jonathan Waller <waller.j27@gmail.com>
2018-05-22 07:45:28 -0500

    early

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,42 @@
-function reducer(){
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: "false",
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: ''
+}
+
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+
+function reducer(state = initialState, action){
 
 }
+
+export function updateLoanType( loanType ) {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType
+    }
+}
+
+export function updatePropertyType( property ) {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: property
+    }
+}
+
 
 export default reducer;
